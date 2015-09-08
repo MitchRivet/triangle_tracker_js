f04191e654c0a59a31f6546794400aeda828abce
Kevin Tokheim & Mitch Rivet <student@epicodus.com>
2015-09-08 14:13:18 -0700

    add triangle verification

--- js/scripts.js
+++ js/scripts.js
@@ -25,28 +25,43 @@ var scaleneTriangle = function(s1, s2, s3) {
 $(document).ready(function() {
 
     $("form#triangle-tracker").submit(function(event) {
-        var side1 = ($("input#side1").val());
-        var side2 = ($("input#side2").val());
-        var side3 = ($("input#side3").val());
+        var side1 = parseInt($("input#side1").val());
+        var side2 = parseInt($("input#side2").val());
+        var side3 = parseInt($("input#side3").val());
 
         var result1 = equilateralTriangle(side1, side2, side3);
         var result2 = isoscelesTriangle(side1, side2, side3);
         var result3 = scaleneTriangle(side1, side2, side3);
 
-        if (result1 === true) {
+
+        if ((side1 >= side2 + side3) || (side2 >= side3 + side1) || (side3 >= side2 + side1))
+        {
+            $(".not_triangle").text("not a triangle");
+            $(".isosceles").empty();
+            $(".equilateral").empty();
+            $(".scalene").empty();
+
+        }
+        else if (result1 === true)
+        {
             $(".equilateral").text("equilateral");
             $(".scalene").empty();
             $(".isosceles").empty();
+            $(".not_triangle").empty();
         }
-        else if (result2 === true) {
+        else if (result2 === true)
+        {
             $(".isosceles").text("isosceles");
             $(".equilateral").empty();
             $(".scalene").empty();
+            $(".not_triangle").empty();
         }
-        else if (result3 === true) {
+        else if (result3 === true)
+        {
             $(".scalene").text("scalene");
             $(".isosceles").empty();
             $(".equilateral").empty();
+            $(".not_triangle").empty();
         }
 
 
